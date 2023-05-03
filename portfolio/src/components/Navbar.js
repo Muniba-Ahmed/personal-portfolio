e7e940e96330d994799493d5e622efc4abbc572e
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-03 11:55:42 -0700

    added a navbar

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="toggleButton">
+        <button> </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
