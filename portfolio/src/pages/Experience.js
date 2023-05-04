0b32667c0ff0b17c408db2d88537b441ee85e737
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-04 13:27:10 -0700

    adding elements on experience timeline

--- portfolio/src/pages/Experience.js
+++ portfolio/src/pages/Experience.js
@@ -1,8 +1,24 @@
 //rfce
+
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className="experience">
+      <VerticalTimeline lineColor="#3e497a">
+        <VerticalTimelineElement
+          className="vertical-timeline-element--education"
+          date="2014-2018"
+          iconStyle={{ background: "#3e497a", color: "#fff" }}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 }
 
 export default Experience;
