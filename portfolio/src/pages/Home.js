a52c6bf5c50ee3355aca43dd5325b5b666876bda
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-03 23:24:23 -0700

    finished up Home.css

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -3,7 +3,7 @@ import GitHubIcon from "@material-ui/icons/GitHub";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import EmailIcon from "@material-ui/icons/Email";
-// import "../styles/Footer.css";
+import "../styles/Home.css";
 
 function Home() {
   return (
@@ -20,7 +20,30 @@ function Home() {
       </div>
       <div className="skills">
         <h1>Skills</h1>
-        <ol></ol>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>
+              JavaScript, React.js, Next.js, ChakraUI, MaterialUI, Styled
+              Components, Redux, Hooks, Axios, Context API, Jest, Cypress, HTML,
+              CSS
+            </span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>
+              Node.js, Express.js, Knex, SQLite3, GitHub, VS Code, Vercel,
+              Heroku
+            </span>
+          </li>
+          <li className="item">
+            <h2>Additional Skills</h2>
+            <span>
+              Git CLI, Data Structures and Algorithms, Object Oriented
+              Programming, Debugging, Troubleshooting
+            </span>
+          </li>
+        </ol>
       </div>
     </div>
   );
