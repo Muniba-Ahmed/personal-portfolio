66c3e887cac8571894d6a5c2439bf7cddc5b26a7
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-03 21:27:59 -0700

    populating Home.js & created Home.css

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+// import "../styles/Footer.css";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, My Name is Muniba</h2>
+        <div className="prompt">
+          <p>A software developer with a passion for learning and creating.</p>
+          <GitHubIcon />
+          <TwitterIcon />
+          <LinkedInIcon />
+          <EmailIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol></ol>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
