b01cf4cc28561493965d75b00b53618e145d2d0b
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-04 22:13:33 -0700

    adding routes for indiv projs w/ ProjectDisplay.js

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import jobTracker2 from "../assets/jobTracker2.png";
-import nasa from "../assets/nasa.png";
-import toDoList from "../assets/toDoList.png";
-import eCom2 from "../assets/eCom2.png";
+import { projectList } from "../helpers/ProjectList";
 import "../styles/Projects.css";
 
 function Projects() {
@@ -11,10 +8,9 @@ function Projects() {
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        <ProjectItem name="Job Tracker" image={jobTracker2} />
-        <ProjectItem name="Nasa Photo Of the Day" image={nasa} />
-        <ProjectItem name="To-Do List" image={toDoList} />
-        <ProjectItem name="eCommerece Website" image={eCom2} />
+        {projectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
       </div>
     </div>
   );
