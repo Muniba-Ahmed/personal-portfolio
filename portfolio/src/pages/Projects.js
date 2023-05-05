6655383f60de6b248feae5c15a9a98e795098e18
Muniba Ahmed <mahmed93@berkeley.edu>
2023-05-04 17:23:50 -0700

    added images of mock projects

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,7 +1,20 @@
 import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import jobTracker from "../assets/jobTracker.png";
+import nasa from "../assets/nasa.png";
+import toDoList from "../assets/toDoList.png";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        <ProjectItem name="Job Tracker" image={jobTracker} />
+        <ProjectItem name="Nasa Photo Of the Day" image={nasa} />
+        <ProjectItem name="To-Do List" image={toDoList} />
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
